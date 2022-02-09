3fba9dfa5075d009299ab0f8e6734f5139c5f235
Snehil Banawa <snehilbanawal@outlook.com>
2022-02-09 17:27:29 +0530

    require

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from django.contrib.messages import constants as messages
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
