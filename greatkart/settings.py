81d6b80eeaddd37b340f73c3d8b38dcf7321b93a
Snehil Banawa <snehilbanawal@outlook.com>
2022-02-09 17:15:04 +0530

    database_patch

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -84,13 +84,22 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd6gp1l7ff36ocn',
+        'USER': 'qtzsqqzlovjoss',
+        'PASSWORD': 'a29151d7100be8da5f4fa31012e64cb4c627da9834c92407256600a903403b15',
+        'HOST': 'ec2-52-208-254-158.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
